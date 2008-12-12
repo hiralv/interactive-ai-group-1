c1b72a46f02fcc2a7729cba877c8f36cc3018d4d
pathfind3r@carolina.rr.com <pathfind3r@carolina.rr.com@ec66423a-a114-11dd-9b86-41075523de00>
2008-12-12 03:11:34 +0000

    Added 3 lives,  working score, ...

--- trunk/WastedSea/Map.cs
+++ trunk/WastedSea/Map.cs
@@ -8,10 +8,8 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Storage;
 using Microsoft.Xna.Framework.Content;
 
-namespace WastedSea
-{
-    enum SquareType
-    {
+namespace WastedSea {
+    enum SquareType {
         OIL,
         SEA,
         BOAT,
@@ -19,17 +17,14 @@ namespace WastedSea
     }
 
 
-    public class MapSpace
-    {
+    public class MapSpace {
         SquareType type;
 
-        public MapSpace()
-        {
+        public MapSpace() {
             type = SquareType.DEBRIS;
         }
 
-        public void Draw()
-        {
+        public void Draw() {
 
         }
     }
@@ -37,37 +32,33 @@ namespace WastedSea
     /// <summary>
     /// This is a game component that implements IUpdateable.
     /// </summary>
-    public class Map : Microsoft.Xna.Framework.GameComponent
-    {
-        
+    public class Map : Microsoft.Xna.Framework.GameComponent {
+
         public MapSpace[,] map_array;
         public int[,] map_type_array;
 
         public Texture2D sea, oil, sky, sea_floor, sea_top, water;
 
-        public Map(Game game) : base(game)
-        {
+        public Map(Game game)
+            : base(game) {
             LoadMap();
 
             map_array = new MapSpace[24, 32];
 
-            for (int y = 0; y < 24; y++)
-            {
-                for (int x = 0; x < 32; x++)
-                {
+            for(int y = 0; y < 24; y++) {
+                for(int x = 0; x < 32; x++) {
                     map_array[y, x] = new MapSpace();
                 }
             }
         }
 
-        public void Initialize(GraphicsDevice Device, ContentManager Content)
-        {
+        public void Initialize(GraphicsDevice Device, ContentManager Content) {
             sea = Content.Load<Texture2D>(@"Sea");
             oil = Content.Load<Texture2D>(@"Oil");
             sky = Content.Load<Texture2D>(@"Sky");
             sea_floor = Content.Load<Texture2D>(@"SeaFloor");
             sea_top = Content.Load<Texture2D>(@"SeaTop");
-            water = Content.Load<Texture2D>(@"water");
+            //water = Content.Load<Texture2D>(@"water");
 
             base.Initialize();
         }
@@ -76,57 +67,46 @@ namespace WastedSea
         /// Allows the game component to update itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        public override void Update(GameTime gameTime)
-        {
+        public override void Update(GameTime gameTime) {
             // TODO: Add your update code here
 
             base.Update(gameTime);
         }
 
-        public void Draw(SpriteBatch spriteBatch)
-        {
+        public void Draw(SpriteBatch spriteBatch) {
             spriteBatch.Begin();
-            spriteBatch.Draw(water, new Rectangle(0, 98, water.Width, water.Height), Color.White);
+           // spriteBatch.Draw(water, new Rectangle(0, 98, water.Width, water.Height), Color.White);
 
-            for (int y = 0; y < 24; y++)
-            {
-                for (int x = 0; x < 32; x++)
-                {
-                   // map_array[y, x].Draw();
+            for(int y = 0; y < 24; y++) {
+                for(int x = 0; x < 32; x++) {
+                    // map_array[y, x].Draw();
                     Texture2D tile;
-                    switch(map_type_array[y,x])
-                    {
-                        case 0:
-                            {
+                    switch(map_type_array[y, x]) {
+                        case 0: {
                                 tile = sky;
                                 break;
                             }
-                        case 1:
-                            {
-                                //tile = sea;
-                                tile = null;
+                        case 1: {
+                                tile = sea;
+                                //tile = null;
                                 break;
                             }
-                        case 4:
-                            {
+                        case 4: {
                                 tile = oil;
                                 break;
                             }
-                        case 5:
-                            {
+                        case 5: {
                                 tile = sea_floor;
                                 break;
                             }
-                        case 6:
-                            {
-                                //tile = sea_top;
-                                tile = null;
+                        case 6: {
+                                tile = sea_top;
+                                //tile = null;
                                 break;
                             }
-                        default:
-                            {
-                                //tile = sea;
-                                tile = null;
+                        default: {
+                                tile = sea;
+                                //tile = null;
                                 break;
                             }
                     }
@@ -138,8 +118,7 @@ namespace WastedSea
             spriteBatch.End();
         }
 
-        public void LoadMap()
-        {
+        public void LoadMap() {
             map_type_array = new int[24, 32] {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                                               {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0},
                                               {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0},
